40016053aad14747895ae6d0a6ba69615067fbd0
Stefan <107488671+steff-mat@users.noreply.github.com>
2022-11-04 22:33:13 +0100

    draft

---
+++ script.js
@@ -0,0 +1,6 @@
+const cardContainer = document.querySelector(".container");
+const cards = document.querySelectorAll(".frame");
+
+for (i = 0; i < cardContainer.length; i++) {
+  console.log(cardContainer[i]);
+}
